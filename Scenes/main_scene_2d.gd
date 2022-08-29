2429b592b16db74d9a522ef5369caed2032484da
Sepeth <sepeth@gmail.com>
2022-08-29 14:12:42 +0300

    Add a 2D character and make it fall from the center of the screen

--- Scenes/main_scene_2d.gd
+++ Scenes/main_scene_2d.gd
@@ -2,9 +2,10 @@ extends Node2D
 
 
 func _ready():
-	pass
+    # Move player to center of screen
+    $Player.position = get_viewport_rect().size / 2
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+    pass
