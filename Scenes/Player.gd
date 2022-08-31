637f7f1fe9e73ae2584225711d8c413028e57b6f
Sepeth <sepeth@gmail.com>
2022-08-31 08:51:26 +0300

    Move the cat back to the beach if staying idle
    If no key is pressed, the cat moves backwards with the half speed.
    
    I also changed motion_mode to Floating (from Grounded) on the CharacterBody2D object. I don't know what this does yet, but it made sense to change it.

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
 @export var speed = 100
-
+@export var float_factor_x = 0.5
 
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
 		position.x += speed * delta
 	elif Input.is_action_pressed("ui_left"):
 		position.x -= speed * delta
+	else:
+		position.x -= speed * delta * float_factor_x
