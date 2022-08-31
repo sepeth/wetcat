153b88e374a37ed10ed7ca44473b7b9f43c6c404
Sepeth <sepeth@gmail.com>
2022-08-31 22:48:48 +0300

    Add fish that jump
    Animation looks a bit static, I need to add tween, and a bit of path variation
    (all fish jump projection is the same).

---
+++ Nodes/Fishes.gd
@@ -0,0 +1,32 @@
+extends Node
+
+const JumpingFishScene = preload("res://Scenes/JumpingFish.tscn")
+const padding = 200
+
+@onready var timer = Timer.new()
+@onready var rng = RandomNumberGenerator.new()
+
+# HACK: parent should set this
+@onready var box_size = get_parent().get_viewport_rect().size
+
+var last_fish = null
+
+
+func _ready():
+	add_child(timer)
+	timer.connect("timeout", _on_Timer_timeout)
+	timer.set_wait_time(1.0)
+	timer.set_one_shot(false)
+	timer.start()
+
+
+func _on_Timer_timeout():
+	if last_fish:
+		remove_child(last_fish)
+	var new_wait = rng.randi_range(1, 4)
+	timer.set_wait_time(new_wait)
+	last_fish = JumpingFishScene.instantiate()
+	last_fish.position.x = rng.randi_range(padding, box_size.x - padding)
+	last_fish.position.y = rng.randi_range(padding, box_size.y - padding)
+	add_child(last_fish)
+
