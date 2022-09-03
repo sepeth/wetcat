c4bc00fc01758c2cdc9d8172a8b4fb664106fc8d
Sepeth <sepeth@gmail.com>
2022-09-03 13:12:40 +0300

    Add fish more often

--- Scenes/main_scene_2d.gd
+++ Scenes/main_scene_2d.gd
@@ -8,20 +8,17 @@ const JumpingFishScene = preload("res://Scenes/JumpingCarp.tscn")
 
 func _ready():
 	add_child(fish_timer)
-	fish_timer.connect("timeout", _on_Timer_timeout)
+	fish_timer.connect("timeout", add_fish)
 	fish_timer.set_wait_time(3.0)
-	fish_timer.set_one_shot(false)
 	fish_timer.start()
 
 
-func _on_Timer_timeout():
-	add_fish()
 
 
 func add_fish():
 	var padding = 200
 	var box_size = get_viewport_rect().size
-	var new_wait = rng.randi_range(3, 6)
+	var new_wait = rng.randi_range(0, 3)
 	fish_timer.set_wait_time(new_wait)
 	var fish = JumpingFishScene.instantiate()
 	fish.got_caught.connect($Player._on_player_eat_fish)
